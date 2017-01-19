51a914a65bdd222a2105a0466e117bfd7d340016
Andreas Fabri <andreas.fabri@geometryfactory.com>
2017-01-19 16:34:50 +0100

    Use typedef int BOOL generally for VC++

--- LASzip/src/mydefs.hpp
+++ LASzip/src/mydefs.hpp
@@ -66,7 +66,7 @@ typedef long long          I64;
 typedef float              F32;
 typedef double             F64;
 
-#if defined(_MSC_VER) && (_MSC_VER < 1300) || defined (__MINGW32__)
+#if defined(_MSC_VER) || defined (__MINGW32__)
 typedef int                BOOL;
 #else
 typedef bool               BOOL;
