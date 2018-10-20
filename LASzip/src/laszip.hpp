f3b628bbd3ca3a85e622d110deedbb42ad6818e4
Martin Isenburg <martin,isenburg@rapidlasso.com>
2018-10-20 10:21:59 +0200

    upped to 3.2 r7 for rare bug in LASinterval::merge_intervals()

--- LASzip/src/laszip.hpp
+++ LASzip/src/laszip.hpp
@@ -25,6 +25,7 @@
 
   CHANGE HISTORY:
 
+    20 October 2018 -- upped to 3.2 r7 for rare bug in LASinterval::merge_intervals()
      5 October 2018 -- upped to 3.2 r6 for corrected 'is_empty' return value
     28 September 2018 -- upped to 3.2 r5 for fix in extended classification writing 
      9 February 2018 -- minor version increment as it can read v4 compressed items
@@ -68,8 +69,8 @@ typedef long long SIGNED_INT64;
 
 #define LASZIP_VERSION_MAJOR                3
 #define LASZIP_VERSION_MINOR                2
-#define LASZIP_VERSION_REVISION             6
-#define LASZIP_VERSION_BUILD_DATE      181005
+#define LASZIP_VERSION_REVISION             7
+#define LASZIP_VERSION_BUILD_DATE      181020
 
 #define LASZIP_COMPRESSOR_NONE              0
 #define LASZIP_COMPRESSOR_POINTWISE         1
