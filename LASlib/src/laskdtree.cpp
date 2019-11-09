493bf7d3ec22d58530daa6bf7843b5f2d4766bd3
Martin Isenburg <martin@rapidlasso.com>
2019-11-09 23:50:55 +0100

    small cast for 32 versus 64 bit compiler warning

--- LASlib/src/laskdtree.cpp
+++ LASlib/src/laskdtree.cpp
@@ -295,7 +295,7 @@ void LASkdtreeRectangles::overlap_rectangles(LASkdtreeRectanglesNode* node, I32 
 
 void LASkdtreeRectangles::print_overlap()
 {
-  fprintf(stderr, "overlap elements: %d\n", overlap_set->size());
+  fprintf(stderr, "overlap elements: %u\n", (U32)overlap_set->size());
   my_index_set::iterator set_element = overlap_set->begin();
   while (TRUE)
   {
