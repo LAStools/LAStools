5d08c332789229fb423cfd506ec8b1ff6c5e5915
zjyhjqs <zjyhjqs@Hotmail.com>
2024-08-06 00:00:24 +0800

    Add namespace for exported target. Target with namespace is useful to prevent empty link. (see CMake policy: CMP0028)

--- LASlib/src/laslib-config.cmake
+++ LASlib/src/laslib-config.cmake
@@ -1,6 +1,8 @@
-get_filename_component(SELF_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
-include(${SELF_DIR}/laslib-targets.cmake)
-get_filename_component(LASlib_INCLUDE_DIRS "${SELF_DIR}/../../../include/LASlib" ABSOLUTE)
-set_property(TARGET LASlib PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${LASlib_INCLUDE_DIRS})
+include("${CMAKE_CURRENT_LIST_DIR}/laslib-targets.cmake")
+add_library(LAStools::LASlib INTERFACE IMPORTED)
+target_link_libraries(LAStools::LASlib INTERFACE LASlib)
+
+get_target_property(LASlib_INCLUDE_DIRS LASlib INTERFACE_INCLUDE_DIRECTORIES)
+set(LASlib_LIBRARIES LAStools::LASlib)
 
 set(LASlib_FOUND true)
